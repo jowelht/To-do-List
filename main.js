323799e1246699ad0e76490b0805b487c5537bc9
jowelht <jowel@hastechit.com>
2022-11-22 12:10:51 +0600

    post update complete

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const displyCard = function (data) {
             <li class="d-flex list align-items-center" data-id="${item?.id}">
                 <span>${item?.id} - ${item?.text}</span> 
                 <span class="end">
-                    <button class="btn btn-success" id="#update-modal" data-bs-toggle="modal" data-bs-target="#update-modal">Edit</button> 
+                    <button class="btn btn-success get-update" id="#update-modal" data-bs-toggle="modal" data-bs-target="#update-modal">Edit</button> 
                     <button class="delete-btn btn btn-danger">Delete</button>
                 </span>
             </li>
@@ -76,3 +76,43 @@ todoListWrap.addEventListener('click', async function(e){
     }
 })
 
+// Get update
+
+const updateText = document.querySelector(".input-fild .form-control")
+const updateButon = document.querySelector(".create-submit-btn")
+
+todoListWrap.addEventListener('click', async  function(e){
+    e.preventDefault()
+    const targetBtn = e.target.classList.contains('get-update')
+    const id = e.target.closest(".list").dataset.id
+
+    const res = await fetch(`${apiEndPoint}/${id}`)
+    const post = await res.json()
+
+    if(targetBtn){
+        updateText.value = post.text
+        updateButon.value = post.id
+    }
+
+})
+// Update json create
+const getUpdate = async function(updatePost, id){
+    const res = await fetch(`${apiEndPoint}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatePost),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    await res.json()
+}
+// post update
+updateButon.addEventListener('click', async function(e){
+    e.preventDefault()
+    const id = e.target.value
+    const post = {
+        text: updateText.value
+    }  
+    await getUpdate(post, id)
+    getDisplyPost()
+})
