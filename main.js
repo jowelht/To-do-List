9a1c3e444058a6275edb1271778ca2b7aa023819
jowelht <jowel@hastechit.com>
2022-11-22 10:34:48 +0600

    delete method aedded

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 
 const apiEndPoint = "http://localhost:3000/user";
-const todoListWrap = document.querySelector(".todo-list-wrap ul")
+const todoListUL = document.querySelector(".todo-list-wrap ul")
+const todoListWrap = document.querySelector(".todo-list-wrap")
 const input = document.querySelector(".single-box input")
 const submitBtn = document.querySelector(".submit-button")
 
+// Disply Card UI
 const displyCard = function (data) {
-    todoListWrap.innerHTML = ""
+    todoListUL.innerHTML = ""
     data.map(item => {
         const html = `
-            <li class="d-flex list">
-                <span>${item?.text}</span> <span class="end"><button>Edit</button> <button>Delete</button></span>
+            <li class="d-flex list" data-id="${item?.id}">
+                <span>${item?.id} - ${item?.text}</span> <span class="end"><button>Edit</button> <button class="delete-btn">Delete</button></span>
             </li>
         `
-        todoListWrap.insertAdjacentHTML("afterbegin", html)
+        todoListUL.insertAdjacentHTML("afterbegin", html)
     })
     
 }
 
+// Get Display Post 
 const getDisplyPost = async function(){
     const res = await fetch(apiEndPoint)
     if(!res.ok){
@@ -25,8 +28,9 @@ const getDisplyPost = async function(){
     const posts = await res.json()
     displyCard(posts)
 }
-
 getDisplyPost()
+
+// Get Post 
 const getPost = async function(newPost) {
     const res = await fetch(apiEndPoint, {
         method: "POST",
@@ -38,6 +42,7 @@ const getPost = async function(newPost) {
     await res.json()
     getDisplyPost()
 }
+// Submit post
 submitBtn.addEventListener("click", async function(e){
     if(input.value === "") return false
     const post = {
@@ -45,4 +50,24 @@ submitBtn.addEventListener("click", async function(e){
     }
     await getPost(post)
     input.value = ""
+})
+
+
+// Get Delete
+const getDelete = async function (id){
+    const response = await fetch(`${apiEndPoint}/${id}`, {
+        method: "DELETE",
+    })
+    await response.json()
+}
+
+todoListWrap.addEventListener('click', async function(e){
+    const targetBtn = e.target.classList.contains('delete-btn')
+    if(targetBtn){
+        const id = e.target.closest(".list").dataset.id
+        await getDelete(id)
+        // DOM Item Remove
+        getDisplyPost()
+        // e.target.closest(".list").remove()
+    }
 })
